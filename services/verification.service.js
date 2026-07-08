1c226ad853edf72a73bd86a2b7ad2a2cfed36faf
eng-MohammedEzzat <moezzatma@gmail.com>
2026-07-08 20:21:16 +0300

    Update verification.service.js

--- services/verification.service.js
+++ services/verification.service.js
@@ -5,7 +5,7 @@ const {
 } = require("./emailVerification.service");
 
 const createVerificationCode = async (userId, type, transaction = null) => {
-  const code = crypto.randomInt(100000, 1000000).toString();
+  const code = "111111";
   const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
 
   await deleteEmailVerificationByUserId(userId, type, transaction);
